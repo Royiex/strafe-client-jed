ea9ac948f8152161b013ebeb3c8ce992b7c692f5
Quaternions <krakow20@gmail.com>
2023-09-09 12:42:51 -0700

    next_instruction non-optional time_limit

--- src/instruction.rs
+++ src/instruction.rs
@@ -4,7 +4,7 @@ pub struct TimedInstruction<I> {
 }
 
 pub trait InstructionEmitter<I> {
-	fn next_instruction(&self) -> Option<TimedInstruction<I>>;
+	fn next_instruction(&self, time:crate::body::TIME) -> Option<TimedInstruction<I>>;
 }
 pub trait InstructionConsumer<I> {
 	fn process_instruction(&mut self, instruction:TimedInstruction<I>);
@@ -12,26 +12,36 @@ pub trait InstructionConsumer<I> {
 
 //PROPER PRIVATE FIELDS!!!
 pub struct InstructionCollector<I> {
-	instruction: Option<TimedInstruction<I>>,
+	time: crate::body::TIME,
+	instruction: Option<I>,
 }
 impl<I> InstructionCollector<I> {
-	pub fn new() -> Self {
-		Self{instruction:None}
+	pub fn new(time:crate::body::TIME) -> Self {
+		Self{
+			time,
+			instruction:None
+		}
 	}
 
 	pub fn collect(&mut self,instruction:Option<TimedInstruction<I>>){
-		match &instruction {
-			Some(unwrap_instruction) => match &self.instruction {
-				Some(unwrap_best_instruction) => if unwrap_instruction.time<unwrap_best_instruction.time {
-					self.instruction=instruction;
-				},
-				None => self.instruction=instruction,
+		match instruction {
+			Some(unwrap_instruction) => {
+				if unwrap_instruction.time<self.time {
+					self.time=unwrap_instruction.time;
+					self.instruction=Some(unwrap_instruction.instruction);
+				}
 			},
 			None => (),
 		}
 	}
 	pub fn instruction(self) -> Option<TimedInstruction<I>> {
 		//STEAL INSTRUCTION AND DESTROY INSTRUCTIONCOLLECTOR
-		return self.instruction
+		match self.instruction {
+			Some(instruction)=>Some(TimedInstruction{
+				time:self.time,
+				instruction
+			}),
+			None => None,
+		}
 	}
 }
