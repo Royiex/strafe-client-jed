bb7ccd97bb22d7ed2d71f58af2ca4886727b5c97
Quaternions <krakow20@gmail.com>
2023-09-22 19:41:27 -0700

    wip

--- src/load_roblox.rs
+++ src/load_roblox.rs
@@ -10,16 +10,27 @@ fn class_is_a(class: &str, superclass: &str) -> bool {
 	}
 	return false
 }
-
-pub fn get_objects(dom:rbx_dom_weak::WeakDom, superclass: &str) -> Result<std::vec::Vec<rbx_dom_weak::Instance>, Box<dyn std::error::Error>> {
-	let mut objects = std::vec::Vec::<rbx_dom_weak::Instance>::new();
-	//move matching instances into objects
-	let (_,mut instances) = dom.into_raw();
-	for (_,instance) in instances.drain() {
-		if class_is_a(instance.class.as_str(), superclass) {
-			objects.push(instance);
+fn recursive_collect_superclass(objects: &mut std::vec::Vec<rbx_dom_weak::types::Ref>,dom: &rbx_dom_weak::WeakDom, instance: &rbx_dom_weak::Instance, superclass: &str){
+	for &referent in instance.children() {
+		if let Some(c) = dom.get_by_ref(referent) {
+			if class_is_a(c.class.as_str(), superclass) {
+				objects.push(c.referent());//copy ref
+			}
+			recursive_collect_superclass(objects,dom,c,superclass);
 		}
 	}
+}
+fn get_texture_refs(dom:&rbx_dom_weak::WeakDom) -> Vec<rbx_dom_weak::types::Ref>{
+	let mut objects = std::vec::Vec::new();
+	recursive_collect_superclass(&mut objects, dom, dom.root(),"Decal");
+	//get ids
+	//clear vec
+	//next class
+	objects
+}
+pub fn get_objects(dom:rbx_dom_weak::WeakDom, superclass: &str) -> Result<std::vec::Vec<rbx_dom_weak::types::Ref>, Box<dyn std::error::Error>> {
+	let mut objects = std::vec::Vec::new();
+	recursive_collect_superclass(&mut objects, &dom, dom.root(),superclass);
 
 	return Ok(objects)
 }
