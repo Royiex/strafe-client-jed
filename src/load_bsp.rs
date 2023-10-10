a5941e24efa4e7386b68d33c112d10999ac1ab78
Quaternions <krakow20@gmail.com>
2023-10-09 21:37:07 -0700

    lib expects u8 slice

--- src/load_bsp.rs
+++ src/load_bsp.rs
@@ -1,9 +1,16 @@
-pub fn generate_indexed_models<R:std::io::Read+std::io::Seek>(input:R) -> crate::model::IndexedModelInstances{
+pub fn generate_indexed_models<R:std::io::Read+std::io::Seek>(input:&mut R) -> crate::model::IndexedModelInstances{
 	let mut spawn_point=crate::integer::Planar64Vec3::ZERO;
 
 	let mut indexed_models=Vec::new();
 
-	match bsp::Bsp::read(input){
+	let mut s=Vec::new();
+
+	match input.read_to_end(&mut s){
+		Ok(guac)=>println!("readed to string {:?}", guac),
+		Err(e)=>println!("faile {:?}",e),
+	}
+
+	match vbsp::Bsp::read(s.as_slice()){
 		Ok(guac)=>println!("we got the guac {:?}", guac),
 		Err(e)=>println!("rotten {:?}",e),
 	}
@@ -12,5 +19,6 @@ pub fn generate_indexed_models<R:std::io::Read+std::io::Seek>(input:R) -> crate:
 		textures:Vec::new(),
 		models:indexed_models,
 		spawn_point,
+		modes:Vec::new(),
 	}
 }
