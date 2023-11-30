fbae4d9f80fd6c5f5e905e464e340cbb1482b324
Quaternions <krakow20@gmail.com>
2023-11-29 21:21:02 -0800

    delete sweep

--- src/sweep.rs
+++
@@ -1,8 +0,0 @@
-
-//something that implements body + hitbox + transform can predict collision
-impl crate::sweep::PredictCollision for Model {
-	fn predict_collision(&self,other:&Model) -> Option<crate::event::EventStruct> {
-		//math!
-		None
-	}
-}
