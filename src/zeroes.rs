aec82358eeabbd85b3484c0305ec88004fa91602
Quaternions <krakow20@gmail.com>
2023-12-02 03:05:53 -0800

    comment about conceptual failure case

--- src/zeroes.rs
+++ src/zeroes.rs
@@ -9,6 +9,7 @@ pub fn zeroes2(a0:Planar64,a1:Planar64,a2:Planar64) -> Vec<Planar64>{
 	let radicand=a1.get() as i128*a1.get() as i128-a2.get() as i128*a0.get() as i128*4;
 	if 0<radicand {
 		//start with f64 sqrt
+		//failure case: 2^63 < sqrt(2^127)
 		let planar_radicand=Planar64::raw(unsafe{(radicand as f64).sqrt().to_int_unchecked()});
 		//TODO: one or two newtons
 		//sort roots ascending and avoid taking the difference of large numbers
